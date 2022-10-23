da1b39aa0299aa5cde01e44bef92feaaa65a4995
Quang <quangcrazymen@gmail.com>
2022-10-23 12:58:07 +0700

    Update on TH03

---
+++ TH03/Bai4-a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdlib.h>
+
+long id_thread;
+
+void main(){
+    // Print Welcome message
+    char *mssv = "20520720";
+    printf("Welcome to IT007, I am %s\n",mssv); 
+    // Open vi editor
+    pthread_create(&id)
+}
